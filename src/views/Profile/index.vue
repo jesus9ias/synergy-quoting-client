71b6544fd91899a904c3a8d60936e491851f41a1
Jesús Escamilla <jesusescamilla@hotmail.com>
2019-04-18 18:45:38 -0500

    start with profile view

---
+++ src/views/Profile/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <main-layout>
+    <general-section :title="$t('profile.title')">
+      <q-card class="my-card">
+        <q-card-section>
+          profile
+        </q-card-section>
+      </q-card>
+      <q-card class="my-card">
+        <q-card-section>
+          change password
+        </q-card-section>
+      </q-card>
+    </general-section>
+  </main-layout>
+</template>
+
+<script>
+export default {
+  name: 'page-users',
+  data() {
+    return {};
+  },
+};
+</script>
